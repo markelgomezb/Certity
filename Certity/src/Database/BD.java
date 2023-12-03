94d1d501a7996c8a1dbd1b763e3c4cfa72575db1
markelgomezb <markel.g@opendeusto.es>
2023-12-03 20:45:16 +0100

    COMIENXO DE LA BD

---
+++ Certity/src/Database/BD.java
@@ -0,0 +1,49 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BD {
+	
+	public static Connection initBD(String nombreBD) {
+		Connection con = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:"+nombreBD);
+					
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	
+	public static void closeBD(Connection con) {
+		if(con!=null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void crearTablas(Connection con) {
+		String sql = "CREATE TABLE IF NOT EXISTS Persona (dni String, nom String)";
+		
+		try {
+			Statement st = con.createStatement();
+			st.executeUpdate(sql);
+			st.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+}
